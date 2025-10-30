4b7d65051b92166b28edf7195b02302c57dd8c2d
Gaurav <gaurav80raj@gmail.com>
2025-10-30 19:53:09 +0530

    initial commit

---
+++ components/skills/Coursework.jsx
@@ -0,0 +1,70 @@
+const Coursework = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Coursework & Tools</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className='bx bxl-python' ></i>
+
+                    <div>
+                        <h3 className="skills__name">Javascript</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className='bx bxl-c-plus-plus' ></i>
+
+                    <div>
+                        <h3 className="skills__name">C++</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className='bx bxl-java' ></i>
+
+                    <div>
+                        <h3 className="skills__name">React</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i className='bx bxl-gitlab' ></i>
+
+                    <div>
+                        <h3 className="skills__name">Git</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className='bx bxl-visual-studio' ></i>
+
+                    <div>
+                        <h3 className="skills__name">Azure</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className='bx bxl-github' ></i>
+
+                    <div>
+                        <h3 className="skills__name">GitHub</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Coursework
